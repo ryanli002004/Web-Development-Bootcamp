ae5b0913fb3c96aad6bff3e3f1eaccc0b6cb8cd0
ryanli002004 <ryanli002004@gmail.com>
2023-10-24 18:08:56 -0400

    5.3

--- 5.3 Axios/index.js
+++ 5.3 Axios/index.js
@@ -15,7 +15,7 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    res.render("index.ejs", { data: result, });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -26,7 +26,19 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
-
+  try{
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    const result = response.data;
+    console.log(result);
+    const finalResult = result[Math.floor(Math.random()*result.length)];
+    console.log(finalResult);
+    res.render("index.ejs",{data: finalResult,});
+  } catch (error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs",{
+      error: "no activities that match your criteria",
+    });
+  }
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
